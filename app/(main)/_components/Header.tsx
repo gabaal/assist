89acfeab52ad4629d883bd4a84aa6ce1e88da942
Gerald <geraldbaalham@live.co.uk>
2025-03-29 18:02:45 +0000

    1:15 Enhance Header component layout and style user image with rounded corners

--- app/(main)/_components/Header.tsx
+++ app/(main)/_components/Header.tsx
@@ -7,10 +7,16 @@ import { useContext } from "react";
 function Header() {
   const { user } = useContext(AuthContext);
   return (
-    <div className="p-3 shadow-sm">
+    <div className="p-3 shadow-sm flex justify-between items-center px-14">
       <Image src={"/logo.svg"} alt="logo" width={40} height={40} />
       {user?.picture && (
-        <Image src={user?.picture} alt="logo" width={40} height={40} />
+        <Image
+          src={user?.picture}
+          alt="logo"
+          width={40}
+          height={40}
+          className="rounded-full"
+        />
       )}
     </div>
   );
